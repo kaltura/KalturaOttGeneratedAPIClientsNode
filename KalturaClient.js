aad57c63f37b2bd8e642d8bc7dcec8bdd605287c
Jenkins <jenkins@kaltura.com>
2019-01-23 15:47:18 +0000

    Auto-generated by Jenkins

--- KalturaClient.js
+++ KalturaClient.js
@@ -8,7 +8,7 @@
 // to do with audio, video, and animation what Wiki platfroms allow them to do with
 // text.
 //
-// Copyright (C) 2006-2018  Kaltura Inc.
+// Copyright (C) 2006-2019  Kaltura Inc.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
@@ -42,8 +42,8 @@ class Client extends kaltura.ClientBase {
 	 */
 	constructor(config) {
 		super(config);
-		this.setApiVersion('5.0.3.18285');
-		this.setClientTag('node:18-11-22');
+		this.setApiVersion('5.0.3.28354');
+		this.setClientTag('node:19-01-23');
 	}
 }
 
