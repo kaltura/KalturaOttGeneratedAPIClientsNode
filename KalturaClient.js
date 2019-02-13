1f32407ae2074a9b254374d16f30ff0afb079c51
Jenkins <jenkins@kaltura.com>
2019-02-13 23:48:36 +0000

    Auto-generated by Jenkins

--- KalturaClient.js
+++ KalturaClient.js
@@ -42,7 +42,7 @@ class Client extends kaltura.ClientBase {
 	 */
 	constructor(config) {
 		super(config);
-		this.setApiVersion('5.1.2.18488');
+		this.setApiVersion('5.1.2.42823');
 		this.setClientTag('node:19-02-13');
 	}
 }
