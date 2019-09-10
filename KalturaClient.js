12ad897b1d27b7069854eca8983009493e972842
Backend CI <ott.rnd.core@kaltura.com>
2019-09-10 07:32:17 +0000

    Auto-generated by Jenkins

--- KalturaClient.js
+++ KalturaClient.js
@@ -42,7 +42,7 @@ class Client extends kaltura.ClientBase {
 	 */
 	constructor(config) {
 		super(config);
-		this.setApiVersion('5.2.5.5883');
+		this.setApiVersion('5.2.5.13438');
 		this.setClientTag('node:19-09-10');
 	}
 }
