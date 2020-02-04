6e06462911b44556c42a58881055e25ccf631412
Backend CI <ott.rnd.core@kaltura.com>
2020-02-04 14:23:33 +0000

    Auto-generated by Jenkins

--- KalturaClient.js
+++ KalturaClient.js
@@ -8,7 +8,7 @@
 // to do with audio, video, and animation what Wiki platfroms allow them to do with
 // text.
 //
-// Copyright (C) 2006-2019  Kaltura Inc.
+// Copyright (C) 2006-2020  Kaltura Inc.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
@@ -42,8 +42,8 @@ class Client extends kaltura.ClientBase {
 	 */
 	constructor(config) {
 		super(config);
-		this.setApiVersion('5.2.3.16350');
-		this.setClientTag('node:19-12-18');
+		this.setApiVersion('5.2.3.5609');
+		this.setClientTag('node:20-02-04');
 	}
 }
 
