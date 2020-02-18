cf2ff38b01b14f107d55e0e89042b36937ad01e7
Backend CI <ott.rnd.core@kaltura.com>
2020-02-18 11:34:21 +0000

    Auto-generated by Jenkins job OTT-BE-Phoenix-Windows, build 2525, branch 531_BEO-7569_TVM_Export_EPG_kSql_filter_dates_failed

--- KalturaClient.js
+++ KalturaClient.js
@@ -42,7 +42,7 @@ class Client extends kaltura.ClientBase {
 	 */
 	constructor(config) {
 		super(config);
-		this.setApiVersion('5.3.2.14683');
+		this.setApiVersion('5.3.2.14673');
 		this.setClientTag('node:20-02-18');
 	}
 }
