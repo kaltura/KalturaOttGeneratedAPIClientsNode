4e552bbd2bac3750b12b004c16cdd393c1cb34c3
Backend CI <ott.rnd.core@kaltura.com>
2020-03-05 16:36:25 +0000

    Auto-generated by Jenkins job OTT-BE-Phoenix-Windows, build 2803, branch 5_3_2

--- KalturaClient.js
+++ KalturaClient.js
@@ -42,7 +42,7 @@ class Client extends kaltura.ClientBase {
 	 */
 	constructor(config) {
 		super(config);
-		this.setApiVersion('5.3.2.14733');
+		this.setApiVersion('5.3.2.14735');
 		this.setClientTag('node:20-03-05');
 	}
 }
