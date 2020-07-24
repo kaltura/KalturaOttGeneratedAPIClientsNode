1c09cd8aed74beb2d4f95ff020dc18043da4bd0d
Backend CI <ott.rnd.core@kaltura.com>
2020-07-24 00:37:44 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/114, branch 5_4_1

--- KalturaClient.js
+++ KalturaClient.js
@@ -42,8 +42,8 @@ class Client extends kaltura.ClientBase {
 	 */
 	constructor(config) {
 		super(config);
-		this.setApiVersion('5.4.0.28206');
-		this.setClientTag('node:20-07-21');
+		this.setApiVersion('5.4.0.28207');
+		this.setClientTag('node:20-07-23');
 	}
 }
 
