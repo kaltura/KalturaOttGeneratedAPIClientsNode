e4616061effd5ecc51df17247cafba27a02e12ec
Backend CI <ott.rnd.core@kaltura.com>
2021-07-15 08:18:17 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/545, branch 661_BEO-10239_Device_Mobility_Policy

--- KalturaClient.js
+++ KalturaClient.js
@@ -42,7 +42,7 @@ class Client extends kaltura.ClientBase {
 	 */
 	constructor(config) {
 		super(config);
-		this.setApiVersion('6.5.0.29190');
+		this.setApiVersion('6.6.0.29206');
 		this.setClientTag('node:21-07-15');
 	}
 }
