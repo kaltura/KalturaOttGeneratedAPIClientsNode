00110ed25df8f0d337f5baabd692536fa19ae78c
Backend CI <ott.rnd.core@kaltura.com>
2022-01-19 11:20:21 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/754, branch 6_9_1

--- KalturaClient.js
+++ KalturaClient.js
@@ -8,7 +8,7 @@
 // to do with audio, video, and animation what Wiki platforms allow them to do with
 // text.
 //
-// Copyright (C) 2006-2021  Kaltura Inc.
+// Copyright (C) 2006-2022  Kaltura Inc.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
@@ -42,8 +42,8 @@ class Client extends kaltura.ClientBase {
 	 */
 	constructor(config) {
 		super(config);
-		this.setApiVersion('6.9.0.29578');
-		this.setClientTag('node:21-11-01');
+		this.setApiVersion('6.9.0.29581');
+		this.setClientTag('node:22-01-19');
 	}
 }
 
